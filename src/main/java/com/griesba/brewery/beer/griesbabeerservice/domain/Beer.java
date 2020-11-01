d94fea5d2b5cbba7cbf603273b9e77ae59b07054
Jean Griesbacher <jnsmplcpt@gmail.com>
2020-11-01 20:36:08 +0100

    Beer model closes #1

--- src/main/java/com/griesba/brewery/beer/griesbabeerservice/domain/Beer.java
+++ src/main/java/com/griesba/brewery/beer/griesbabeerservice/domain/Beer.java
@@ -1,4 +1,46 @@
 package com.griesba.brewery.beer.griesbabeerservice.domain;
 
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
 public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    @Type(type = "uuid-char")
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    private String name;
+
+    private String style;
+
+    @Column(unique = true)
+    private String upc;
+
+    private double price;
+
+    private Integer quantityToBrew;
+
+    private Integer minOnHand;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp modificationDate;
 }
