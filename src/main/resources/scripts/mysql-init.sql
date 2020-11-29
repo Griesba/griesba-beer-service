dacf728fcbf24be8e9ea31de0392ce4668e2db10
griesba <griesba.pat@hotmail.com>
2020-11-29 23:17:52 +0100

    configure local mysql profile beer service closes #20

--- src/main/resources/scripts/mysql-init.sql
+++ src/main/resources/scripts/mysql-init.sql
@@ -4,7 +4,7 @@ drop user if exists `beerservice`@`%`;
 
 create database if not exists beerservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create user if not exists `beerservice`@`%` identified with mysql_native_password by `password`;
+create user if not exists `beer_service`@`%` identified with mysql_native_password by `password`;
 
 grant select, insert, update, delete, create, drop, references, index, alter, execute, create view, show view,
 create routine, alter routine, event, trigger on `beerservice`.* to `beerservice`@`%`;
