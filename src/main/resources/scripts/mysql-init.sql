ab798d643841d7765a1d008dd4fade4c574aedba
griesba <griesba.pat@hotmail.com>
2020-12-02 22:40:38 +0100

    MySQL database connection pooling configuration with Hikari closes #21

--- src/main/resources/scripts/mysql-init.sql
+++ src/main/resources/scripts/mysql-init.sql
@@ -1,12 +1,12 @@
 drop database if exists beerservice;
 
-drop user if exists `beerservice`@`%`;
+drop user if exists `beer_service`@`%`;
 
 create database if not exists beerservice CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
-create user if not exists `beer_service`@`%` identified with mysql_native_password by `password`;
+create user if not exists `beer_service`@`%` identified with mysql_native_password by 'password';
 
 grant select, insert, update, delete, create, drop, references, index, alter, execute, create view, show view,
-create routine, alter routine, event, trigger on `beerservice`.* to `beerservice`@`%`;
+create routine, alter routine, event, trigger on `beerservice`.* to `beer_service`@`%`;
 
 flush privileges;
